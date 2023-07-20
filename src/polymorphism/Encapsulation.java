f1bd3c90a9bd0afae9ced4d6d25b2cd944f8e878
Lee Hyori <gyfl5542@naver.com>
2023-07-20 16:44:09 +0900

    java practice

---
+++ src/polymorphism/Encapsulation.java
@@ -0,0 +1,67 @@
+package polymorphism;
+
+
+
+// 캡슐화의 필요성
+// 변수를 아주 지멋대로 바꿔버릴 수 있음. 
+// 내가 원하는 대로 변수를 제어할거란 생각은 버리자. 
+// 사용자는 내가 원하는 방식대로 안 할거임. 
+// 애초에 강제성을 부여해서 그렇게 할 수 밖에 없게 해야 됨
+
+// 캡슐화의 방법
+// -> 멤버 변수에 직접 접근할 수 없게 하고 메서드 통해서만 제어할 수 있게 해야 함. 
+// 멤버 변수를 private처리하고 
+// 멤버 변수 값을 변경하는 getter 메서드와 멤버 변수 값을 반환하는 setter함수를 세트로 만들자. 
+
+class Person{
+	private String name;
+	private int age;
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void eat() {
+		System.out.println("냠냠");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
+
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+
+		
+		Person p1 = new Person("hyori", 25);
+		 
+		System.out.println(p1.getAge());
+		p1.setAge(10);
+		System.out.println(p1.getAge());
+		
+		System.out.println(p1.getName());
+		p1.setName("Nari");
+		System.out.println(p1.getName());
+		
+		
+				
+	}
+
+}
---
+++ src/polymorphism/SingletonTest.java
@@ -0,0 +1,48 @@
+package polymorphism;
+
+class Singleton{
+	/*
+	 * 싱글톤 디자인 패턴의 필요성
+	 * 그러니까, static 변수만 있다던가 기능(메서드)만 있는 클래스의 경우는 
+	 * 객체를 여러개 생성해서 사용하는 게 손해잖아. 메모리 낭비란 말이야. 
+	 * 그래서 우리가 이건 객체를 하나만 만들어어서 사용하세요~ 해봐야
+	 * 사용자들은 걍 지맘대로 쓸거란 말이지. 
+	 * 사용자들이 우리 의도대로 잘 쓸거라는 기대를 애초에 하지 말자. 
+	 * 
+	 * 애초에 그냥 그럴 수 밖에 없게 해야 한다. 
+	 * 봐봐, 변수를 원하는대로 제어하기 위해서 변수에 직접 접근하지 못하고
+	 * 메서드를 통해 접근(ex) 값이 0보다 클때만 할당)해서  했던 거 기억나지? 
+	 * 그것처럼 애초에 강제성을 부여해서 다른 방법은 못하게 만들어야 돼. 
+	 * 
+	 * 
+	 * 그럼 객체를 한번만 만들 수 있게 하려면 어떻게 해야 할까?
+	 * 그것에 대한 디자인 패턴이 싱글톤 디자인 패턴이다. 
+	 * 
+	 * 
+	 * 싱글톤 디자인 패턴이란?
+	 * 객체를 오직 하나만 만들어서 사용하기 위해 고안된 디자인 패턴
+	 * 클래스 외부에서는 생성자를 호출하지 못하게 하고
+	 * 클래스 내부에서 객체를 생성해서 반환해줌. 
+	 */		
+		private static Singleton instance = new Singleton();
+		private Singleton() {}
+		
+		public static Singleton getInstance() {
+			return instance;
+		}
+		
+		public void sayHello() {
+			System.out.println("Hello");
+		}
+
+}
+
+public class SingletonTest {
+	public static void main(String[] args) {
+		Singleton sc1 = Singleton.getInstance();
+		Singleton sc2 = Singleton.getInstance();
+		
+		System.out.printf("두 객체는 같은가? %b%n", sc1==sc2);
+		sc1.sayHello();
+	}
+}
